bd7ebd236062ef97e05ba5fba5c69632a627bb21
Terminaliya <terminaliya011@gmail.com>
2022-06-15 17:31:33 +0300

    Task 2
    Task 2

--- MainClass.java
+++ MainClass.java
@@ -2,6 +2,15 @@ import org.junit.Test;
 
 public class MainClass {
 
+    private int class_number = 20;
+    public int getLocalNumber(){
+        return 14;
+    }
+
+    public int getClassNumber (){
+        return this.class_number;
+    }
+
     @Test
     public void mainClassTest(){
         int a = this.getLocalNumber(); //в данном случае можно ведь и без this?
@@ -12,7 +21,13 @@ public class MainClass {
         } else System.out.println("The number is 14");
     }
 
-    public int getLocalNumber(){
-        return 14;
+    @Test
+    public void testGetClassNumber(){
+        int a = getClassNumber();
+
+        if (a > 45){
+            System.out.println("Test passed. Number greater than 45"); // Если это ожидаемый результат. А то я не очень поняла условие проверки
+        } else
+            System.out.println("Test Failed. Number less or equal 45");
     }
 }
